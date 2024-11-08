99504c49c502ff168cde249900ce481d9b54064e
Modassir Imran <modassirimran904@gmail.com>
2024-11-08 22:11:05 +0530

    set frontend url

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const connectDB = require('./config/db');
 
 // CORS Configuration
 app.use(cors({
-  origin: 'http://localhost:3000', // Allow only frontend origin
+  origin: 'https://mern-crud-app-frontend-red.vercel.app/', // Allow only frontend origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
